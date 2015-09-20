80d25797163c025d7ada947ad663b6403ccd1755
Patrick Neschkudla <neschkudla@gmail.com>
2015-09-20 23:41:01 +0200

    mobile support

---
+++ mobile-config.js
@@ -0,0 +1,27 @@
+App.info({
+  id: 'com.neschkudla.patrick.simplebucket',
+  name: 'SimpleBucket',
+  description: 'An easy to use bitbucket repository manager.',
+  author: 'Patrick Neschkudla',
+  email: 'neschkudla@gmail.com',
+  website: 'http://neschkudla.at',
+  version: '0.0.1'
+});
+
+App.icons({
+  'android_ldpi': 'public/images/appicon.png',
+  'android_mdpi': 'public/images/appicon.png',
+  'android_hdpi': 'public/images/appicon.png',
+  'android_xhdpi': 'public/images/appicon.png'
+});
+
+App.launchScreens({
+    'android_ldpi_portrait': 'public/images/mobile/app/assets/iphone/Default.png',
+    'android_ldpi_landscape': 'public/images/mobile/app/assets/iphone/Default-Landscape.png',
+    'android_mdpi_portrait': 'public/images/mobile/app/assets/iphone/Default-Portrait@2x.png',
+    'android_mdpi_landscape': 'public/images/mobile/app/assets/iphone/Default-Landscape@2x.png',
+    'android_hdpi_portrait': 'public/images/mobile/app/assets/iphone/Default-Portrait-736h@3x.png',
+    'android_hdpi_landscape': 'public/images/mobile/app/assets/iphone/Default-Landscape-736h@3x.png',
+    'android_xhdpi_portrait': 'public/images/mobile/app/assets/iphone/Default-Portrait-736h@3x.png',
+    'android_xhdpi_landscape': 'public/images/mobile/app/assets/iphone/Default-Landscape-736h@3x.png'
+});
